a7b40e4ffc13c10f0e774044f3c37c79d0319145
onurcan ertekin <onrcn_ertkn@hotmail.com>
2023-03-26 00:45:03 +0300

    Turn object list into string list with regex clear extensions

---
+++ SuggestNextShow.Entry/Handlers/Helpers/HandleList.cs
@@ -0,0 +1,25 @@
+﻿using StockBridge.Handler;
+
+namespace SuggestNextShow.Entry.Handlers.Helpers
+{
+    /// <summary>
+    /// Handler for List operations
+    /// </summary>
+    public static class HandleLists
+    {
+        /// <summary>
+        /// Turn object list into string list
+        /// </summary>
+        /// <param name="objects"></param>
+        /// <returns></returns>
+        public static List<string?> ObjectListToStringList(List<object> objects)
+        {
+            List<string?> list = new();
+            foreach (object obj in objects)
+            {
+                list.Add(Convert.ToString(obj).ClearUnwantedCharacters());
+            }
+            return list;
+        }
+    }
+}
