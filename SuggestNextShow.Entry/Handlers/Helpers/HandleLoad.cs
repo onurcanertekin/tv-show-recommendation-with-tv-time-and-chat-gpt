63d570352bb5e0199cce1fffd52f2ec02f007a81
onurcan ertekin <onrcn_ertkn@hotmail.com>
2023-03-26 00:28:10 +0300

    add new load method for loading

--- SuggestNextShow.Entry/Handlers/Helpers/HandleLoad.cs
+++ SuggestNextShow.Entry/Handlers/Helpers/HandleLoad.cs
@@ -4,16 +4,43 @@ using CefSharp.OffScreen;
 namespace SuggestNextShow.Entry.Handlers.Helpers
 {
     /// <summary>
-    /// Handler for browser to wait until current page load end
+    /// Handler for browser to wait until frame or loading end
     /// </summary>
     public static class HandleLoad
     {
         /// <summary>
-        /// Wait for the page to finish loading
+        /// Wait for the page to loading state change
         /// </summary>
         /// <param name="browser"></param>
         /// <returns></returns>
-        public static async Task WaitForPageLoadEnd(ChromiumWebBrowser browser)
+        public static async Task WaitForLoadingEnd(ChromiumWebBrowser browser)
+        {
+            var frameLoadTaskCompletionSource = new TaskCompletionSource<bool>();
+            if (browser.IsLoading)
+            {
+                void LoadingStateChangeHandler(object sender, LoadingStateChangedEventArgs e)
+                {
+                    if (e.IsLoading == false)
+                    {
+                        frameLoadTaskCompletionSource.TrySetResult(true);
+                        browser.LoadingStateChanged -= LoadingStateChangeHandler;
+                    }
+                }
+                browser.LoadingStateChanged += LoadingStateChangeHandler;
+                await frameLoadTaskCompletionSource.Task;
+            }
+            else
+            {
+                frameLoadTaskCompletionSource.TrySetResult(true);
+            }
+        }
+
+        /// <summary>
+        /// Wait for the page to finish frame load end
+        /// </summary>
+        /// <param name="browser"></param>
+        /// <returns></returns>
+        public static async Task WaitForFrameLoadEnd(ChromiumWebBrowser browser)
         {
             var frameLoadTaskCompletionSource = new TaskCompletionSource<bool>();
             void FrameLoadEndHandler(object sender, FrameLoadEndEventArgs e)
